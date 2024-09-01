2d80e2e52f24afdd81c4513c769482502192d0f4
Cleg <ewihinnovation@gmail.com>
2024-09-02 00:24:27 +0700

    signup

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from form import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'dff110a589f30f002f8bf7a72cd4254f'
 
 posts= [
     {
@@ -20,11 +23,24 @@ posts= [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html", posts=posts)
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template('about.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
